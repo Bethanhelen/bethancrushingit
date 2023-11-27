73580145cca3e3d01d4aa90459e6e4da0e7f4ec9
Bethan Helen <bethanhelen8@gmail.com>
2023-11-27 23:15:32 +0100

    body content filled

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
+//dc dropdown
 let subMenu = document.getElementById("sub-Menu");
 function toggleMenu(){
   subMenu.classList.toggle("open-menu");
 }
 
+//notification dropdown
 let notMenu = document.getElementById("not-Menu");
 function switchMenu(){
   notMenu.classList.toggle("view-menu");
+}
+
+// //body dropdown
+// const optionMenu = document.querySelector(".select-menu");
+// const selectBtn = optionMenu.querySelector(".bottom");
+// const options = optionMenu.querySelectorAll(".option");
+// const sBtn_text = optionMenu.querySelector(".bottom-top");
+
+// options.forEach(option => {
+//   option.addEventListener("click",()=>{
+//  let selectedOption = option.querySelector(".option-text").innerText;
+
+//  console.log(selectedOption)
+//   })
+  
+// })
+
+//body header hide
+const div = document.getElementById('main');
+const display = 0;
+
+function hideShow(){
+if(display == 1){
+  div.style.display = 'block';
+  display = 0;
+}else{
+  div.style.display = 'none';
+  display = 1;
+}
 }
