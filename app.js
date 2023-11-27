3fa83b3c081d9f5bafc7f694c418687808d5ee4c
Bethan Helen <bethanhelen8@gmail.com>
2023-11-27 16:24:27 +0100

    the notification dopdown

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-let subMenu = document.getElementById("subMenu");
+let subMenu = document.getElementById("sub-Menu");
 function toggleMenu(){
   subMenu.classList.toggle("open-menu");
+}
+
+let notMenu = document.getElementById("not-Menu");
+function switchMenu(){
+  notMenu.classList.toggle("view-menu");
 }
